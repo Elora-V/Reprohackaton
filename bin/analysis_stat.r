50fc11a319f640d8337279849c7d8cff8ba9f62b
Elora-V <elora95.vigo@gmail.com>
2023-10-30 15:21:00 +0100

    comparaison result R

--- bin/analysis_stat.r
+++ bin/analysis_stat.r
@@ -1,32 +1,51 @@
 setwd("~/Documents/M2_AMI2B/Reprohackaton/reprohackathon/bin")
 rm(list = ls())
 
+
+
+#data = 0 si nos données, 1 sinon
+
+
+for (data in c(1,0)){
+
 #R:v4.2.1
 #installation des packages
-install.packages("BiocManager")
-BiocManager::install("DESeq2", force =T )
+#install.packages("BiocManager")
+#BiocManager::install("DESeq2", force =T )
 #BiocManager::install("EnrichmentBrowser")
 #BiocManager::install("org.Staphylococcus_aureus.eg.db")
 #BiocManager::install("clusterProfiler")
 #activer les dépendances
 library("DESeq2")
 library(ggplot2)
-library("EnrichmentBrowser")
-library("KEGGREST")
-library("clusterProfiler")
+#library("EnrichmentBrowser")
+#library("KEGGREST")
+#library("clusterProfiler")
 
 
 ### Counting matrix 
-
+if (data == 0){
 #analyse statistique de la sortie featureCount
 countData = read.table("final_count_matrix.txt", header = TRUE, row.names = 1, sep = "\t")
-
-#countData <- read.table("/Users/fionahak/Documents/m2_ami2b/reprohackathon/reprohackathon/results/COUNTING/final_count_matrix.txt", header = TRUE, row.names = 1, sep = "\t")
 #affecter les conditions
 colDataFrame <- data.frame(
   Sample = colnames(countData),
   Condition = c("Treatment", "Treatment", "Treatment", "Control", "Control", "Control")
 )
+} else {
+# pr celle de l'article
+countData=read.delim("GSE139659_IPvsctrl.complete.xls",header=T,row.names = NULL) # pour tester avec la vrai matrice
+countData=countData[1:2967,]
+rownames(countData)=countData$Name
+countData=countData[,6:11]
+countData= na.omit(countData)
+colDataFrame <- data.frame(
+  Sample = colnames(countData),
+  Condition = c("Control", "Control", "Control","Treatment", "Treatment", "Treatment")
+) 
+}
+
+
 
 ### Genes names matrix
 
@@ -43,10 +62,18 @@ geneName = read.table("GeneSpecificInformation_NCTC8325.tsv", header=T,sep = "\t
 ### Differential expression analysis
 
 ##normalization and dispersion estimation
-#créer l'objet pour des à partir de la matrice
+#créer l'objet pour deseq2 à partir de la matrice
 dds <- DESeqDataSetFromMatrix(countData, colData = colDataFrame, design = ~ Condition)
 dds <- DESeq(dds)
 res <- results(dds)
+#Changement des noms de lignes et Ajout des symboles des genes du tableau de résultat
+rownames(res)=sub("^gene-", "", rownames(res))
+name=geneName[ match(rownames(res),geneName[,1]) , 2]
+indexNa=which(is.na(name)) 
+name[indexNa]=rownames(res)[indexNa]
+res$name=name
+
+
 #adjust p-values using the Benjamini and Hochberg procedure
 res$padj <- p.adjust(res$pvalue, method = "BH")
 #genes with an adjusted p value lower than 0.05 were considered differentially expressed
@@ -54,19 +81,22 @@ res$padj <- p.adjust(res$pvalue, method = "BH")
 de_genes <- subset(res, padj < 0.05)
 #length(de_genes$baseMean) #1487 DEG => 10 de plus que dans l'article
 
-# get name of gene
 
-name=geneName[ match(rownames(res),geneName[,1]) , 2]
-indexNa=which(is.na(name))
-name[indexNa]=rownames(res)[indexNa]
-res$name=name
+
+if (data == 0){
+  RES=res
+  GeneDiff=de_genes
+} else {
+  RES_A=res
+  GeneDiff_A=de_genes 
+}
 
 #### Figure supplémentaire 3 : MA plot
 
 res$log2FoldChange=ifelse(res$log2FoldChange > 4, 4, ifelse(res$log2FoldChange < -4, -4, res$log2FoldChange))
 resdf=data.frame(res)
 
-ggplot(data = resdf, aes(x = log(baseMean), y = log2FoldChange)) +
+maplot=ggplot(data = resdf, aes(x = log(baseMean), y = log2FoldChange)) +
   geom_point(aes(color = padj < 0.05), shape = 16,cex=1) +
   scale_color_manual(values = c("black", "red")) +
   labs(
@@ -94,35 +124,57 @@ transl[,1] <- sub("^(\\S+).*", "\\1", transl[,1]) # on récupère juste le nom d
 transl[,1] <- sub("^sao:", "", transl[,1])
 
 # verification avec un autre fichier qu'on a les bons gènes (à supprimer plus tard)
-Transl2=read.table("geneTranslation2.txt",sep="\t")
-Transl2[,1] <- sub("^\\s+", "", Transl2[,1])  # supprime les premiers espaces
-Transl2[,1] <- sub("^(\\S+).*", "\\1", Transl2[,1]) # recupère le premeir mot
+#Transl2=read.table("geneTranslation2.txt",sep="\t")
+#Transl2[,1] <- sub("^\\s+", "", Transl2[,1])  # supprime les premiers espaces
+#Transl2[,1] <- sub("^(\\S+).*", "\\1", Transl2[,1]) # recupère le premeir mot
 
 ## result for translation
 
-rownames(res)=sub("^gene-", "", rownames(res))
+
 index_transl=match(transl[,1],rownames(res))
 index_transl=na.omit(index_transl) # 3 NA
 res_transl=data.frame(res[index_transl,])
 
 
+if (data == 0){
+  REST=res_transl
+} else {
+  REST_A=res_transl 
+}
 
 # MA plot (traslation)
 
 
-ggplot(data = res_transl, aes(x = log2(baseMean), y = log2FoldChange)) +
-  geom_point(aes(color = padj < 0.05), shape = 16,cex=2) +
+library(ggrepel)
+
+maplot_transl <- ggplot(data = res_transl, aes(x = log2(baseMean), y = log2FoldChange)) +
+  geom_point(aes(color = padj < 0.05), shape = 16, cex = 1.5) +
   scale_color_manual(values = c("black", "red")) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
   labs(
     x = "log(baseMean)",
     y = "log2FoldChange",
-    color = "padj < 0.05"
-  )  +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  labs(title = "MA-plot of translation genes")+
-  theme(plot.title = element_text(hjust = 0.5))
+    color = "padj < 0.05",
+    title = "MA-plot of translation genes"
+  ) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_label_repel(data = subset(res_transl, !grepl("^SAOUHSC", name)),
+                   aes(label = name),
+                   box.padding = unit(0.5, "lines"),
+                   point.padding = unit(0.3, "lines"))
 
+print(maplot_transl)
 
+        
+if (data == 0){
+ titre="MA-plot.pdf"
+} else {
+  titre="MA-plot_article.pdf"
+}
+pdf(titre, width = 10, height = 7)
+print(maplot)     # in the first page of PDF
+print(maplot_transl)     # in the second page of the PDF
+dev.off() 
 
 # 
 # organism_code <- "sao"
@@ -135,4 +187,9 @@ ggplot(data = res_transl, aes(x = log2(baseMean), y = log2FoldChange)) +
 
 #significant_kegg_enrich <- subset(kegg_enrich, padj < 0.05)
 #significant_kegg_enrich
+
+}
+
+
+
 
