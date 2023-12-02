56cf917106fd811cab4bb7068d1059f24b000fd9
Celine Guo <celine.guo@universite-paris-saclay.fr>
2023-12-02 22:14:54 +0000

    Update analysis_stat.r

--- bin/analysis_stat.r
+++ bin/analysis_stat.r
@@ -218,7 +218,13 @@ print(heatmap(cor))
 print(PCAplot)
 print(hist(res$padj))
 print(maplot)     
-print(maplot_transl)    
+print(maplot_transl)   
+
+pdf("Comparaison_resultats.pdf", width = 10, height = 7)
+print(PCAplot)
+print(heatmap(cor))
+print(plotfold)
+
 dev.off() 
   
 }
